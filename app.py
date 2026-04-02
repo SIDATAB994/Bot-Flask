45ab23ccf7711830969cf46b72cae16721f58749
SIDATAB994 <sidatab994@atomicmail.io>
2026-04-02 04:39:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,25 @@
 from flask import Flask, jsonify, request
 from strategy import get_bets_for_date
-import os, datetime
+import datetime, os
 
 app = Flask(__name__)
 
-bets_memory = {}
-bets_date = None
-
 @app.route("/")
 def home():
-    return "Bot actif"
-
-@app.route("/run")
-def run():
-    global bets_memory, bets_date
-    bets_memory = get_bets_for_date()
-    bets_date = datetime.date.today()
-    return jsonify(bets_memory)
+    return "Betting AI actif"
 
 @app.route("/bets")
 def bets():
-    global bets_memory, bets_date
     date_str = request.args.get("date")
+
     target_date = None
     if date_str:
         try:
             target_date = datetime.datetime.strptime(date_str, "%Y-%m-%d").date()
         except:
-            target_date = None
-    if bets_date != target_date or not bets_memory:
-        bets_memory = get_bets_for_date(target_date)
-        bets_date = target_date or datetime.date.today()
-    return jsonify(bets_memory)
+            pass
+
+    return jsonify(get_bets_for_date(target_date))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT",5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
